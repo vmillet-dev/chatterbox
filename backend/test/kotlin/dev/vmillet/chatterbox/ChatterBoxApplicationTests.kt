448bd083e8cccaadf32f8aba5ca70ed194fcf5ba
Valentin MILLET <valentin.millet39@gmail.com>
2024-06-26 23:51:47 +0200

    Add missing dependencies for test module

--- backend/test/kotlin/dev/vmillet/chatterbox/ChatterBoxApplicationTests.kt
+++ backend/test/kotlin/dev/vmillet/chatterbox/ChatterBoxApplicationTests.kt
@@ -3,8 +3,10 @@ package dev.vmillet.chatterbox
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@ContextConfiguration(classes = [ChatterBoxApplicationTests::class])
 @ActiveProfiles("test")
 class ChatterBoxApplicationTests {
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
-	id("org.springframework.boot") version "3.3.1"
-	id("io.spring.dependency-management") version "1.1.5"
-	kotlin("jvm") version "2.0.0"
-	kotlin("plugin.spring") version "2.0.0"
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.springboot.framework)
+	alias(libs.plugins.spring.dep.management)
+//	id("org.springframework.boot") version "3.3.1"
+//	id("io.spring.dependency-management") version "1.1.5"
+//	kotlin("jvm") version "2.0.0"
+//	kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "dev.vmillet"
@@ -19,19 +23,38 @@ repositories {
 }
 
 dependencies {
-	runtimeOnly("org.postgresql:postgresql:42.7.3")
+	implementation(libs.spring.autoconfigure)
+	implementation(libs.spring.web)
+	implementation(libs.spring.webmvc)
+	implementation(libs.spring.websocket)
+	implementation(libs.spring.data.jpa)
+	implementation(libs.fasterxml.jackson)
+	implementation(libs.postgresql)
+	implementation(libs.kotlin.reflect)
+	implementation(libs.hibernate)
+	implementation(libs.jakarta.persistence)
+
+	testImplementation(libs.springboot.test)
+	testImplementation(libs.springboot.jdbc)
+	testImplementation(libs.kotlin.test)
+	testImplementation(libs.h2)
+	testRuntimeOnly(libs.junit.launcher)
+
+
+
+//	runtimeOnly("org.postgresql:postgresql:42.7.3")
 
 	implementation("org.springframework.boot:spring-boot-starter-web:3.3.1")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.1")
 	implementation("org.springframework.boot:spring-boot-starter-websocket:3.3.1")
 
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
-	implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
+//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+//	implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.0")
-	testImplementation("com.h2database:h2")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
+//	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.0")
+//	testImplementation("com.h2database:h2")
+//	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
 kotlin {
