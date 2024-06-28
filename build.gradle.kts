4e448eda689f6f32993ced25b30e5c43149c49bc
Valentin MILLET <34792012+vmillet-dev@users.noreply.github.com>
2024-06-28 09:24:28 +0000

    remove commented conf in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
 	alias(libs.plugins.kotlin.spring)
 	alias(libs.plugins.springboot.framework)
 	alias(libs.plugins.spring.dep.management)
-//	id("org.springframework.boot") version "3.3.1"
-//	id("io.spring.dependency-management") version "1.1.5"
-//	kotlin("jvm") version "2.0.0"
-//	kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "dev.vmillet"
@@ -39,22 +35,6 @@ dependencies {
 	testImplementation(libs.kotlin.test)
 	testImplementation(libs.h2)
 	testRuntimeOnly(libs.junit.launcher)
-
-
-
-//	runtimeOnly("org.postgresql:postgresql:42.7.3")
-
-	implementation("org.springframework.boot:spring-boot-starter-web:3.3.1")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.1")
-	implementation("org.springframework.boot:spring-boot-starter-websocket:3.3.1")
-
-//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
-//	implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
-
-//	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
-//	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.0")
-//	testImplementation("com.h2database:h2")
-//	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
 kotlin {
